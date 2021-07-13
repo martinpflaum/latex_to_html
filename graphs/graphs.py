7cf899a2a98dc11795ff494bfeae040fda4c9bff
Martin99y <martin.pflaum.09.03.1999@gmail.com>
2021-07-13 02:07:28 +0200

    near finished graph

--- graphs/graphs.py
+++ graphs/graphs.py
@@ -6,22 +6,26 @@ source = """
 digraph G {
   { 
     node [margin=0  fontcolor=blue fontsize=12 width=0.8 shape=circle style=filled color=white fixedsize=shape]
-    b [label="a very long label"]
-    a [label="an even longer label"]
+    b [label="X_G"]
+    a [label="\\sum^4_i i^2"]
     c [label="K_\\varphi"]
 
   }
-  a -> c [label="XXX" len=2.0]
+  a -> c [class="inline" label="G_\\varphi" len=2.0]
   c -> b [len=2.0]
   b -> a [len=2.0]
   
 }
 """
+source = source.replace("\\","XXXbackslashXXX")
 
 input = graph_to_svg(source)
+#style="background-color:#ffff;"
+input = input.replace("<text",'<foreignObject class="inline" width="500" height="500"')
+input = input.replace("</text>",'</foreignObject>')
+input = input.replace("XXXbackslashXXX","\\")
+
 #input = input.replace("XXX","<foreignObject width='500' height='500' class='inline'>X \\times Y</foreignObject>")
 create_final_file(input,"test.html")
-
-# %%
 
 # %%
